8f8bab20cb9c2e148aa974a00c1b9a5ae3129be1
crypticGøøse <christogoosen@gmail.com>
2020-07-22 18:42:10 +0200

    Install rollbar dependency (#1)
    * Install rollbar dependency
    
    * Update version

--- setup.py
+++ setup.py
@@ -11,11 +11,12 @@ except ImportError:
 
 setup(
     name='django-q-rollbar',
-    version='0.1.1',
+    version='0.1.2',
     author='Daniel Welch',
     author_email='dwelch2102@gmail.com',
     keywords='django distributed task queue worker scheduler cron redis disque ironmq sqs orm mongodb multiprocessing rollbar',
     packages=['django_q_rollbar'],
+    install_requires=['rollbar>=0.14.0'],
     url='https://django-q.readthedocs.org',
     license='MIT',
     description='A Rollbar support plugin for Django Q',
